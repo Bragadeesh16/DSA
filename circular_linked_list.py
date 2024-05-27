7462d342c53fb068e84dd5c40f34cad0b11634d5
bragadeesh <bragadeesh03294@gamil.com>
2024-05-27 22:03:13 +0530

    circular linked list

---
+++ circular_linked_list.py
@@ -0,0 +1,32 @@
+class node:
+    def __init__(self,data) -> None:
+        self.data = data
+        self.next = None
+
+
+class circular_linked_list:
+    def __init__(self) -> None:
+        self.head = None
+
+    def add_node(self,data):
+        new_node = node(data)
+        if not self.head:
+            self.head = new_node
+            self.head.next = self.head
+        else:
+            new_node.next = self.head
+            current = self.head
+            while current.next != self.head:
+                current = current.next
+            current.next = new_node
+            self.head = new_node
+
+    def print_list(self):
+        current = self.head
+        while current:
+            print(current.data, end=" -> ")
+            current = current.next
+            if current == self.head:
+                break
+        print()
+
